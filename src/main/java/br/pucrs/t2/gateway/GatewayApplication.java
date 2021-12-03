4f4877effac0e7688f1bca11c8306123a4c88021
hcley <cleysonb.oliveira@gmail.com>
2021-12-03 19:28:07 -0300

    adding bean configuration

--- src/main/java/br/pucrs/t2/gateway/GatewayApplication.java
+++ src/main/java/br/pucrs/t2/gateway/GatewayApplication.java
@@ -2,7 +2,11 @@ package br.pucrs.t2.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +16,14 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Configuration
+	public class ApiGatewayConfiguration {
+		@Bean
+		public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+			return builder.routes()
+				.route(p -> p.path("/inventory/**").uri("http://localhost:8081"))
+				.route(p -> p.path("/sales/**").uri("http://localhost:8082"))
+				.build();
+		}
+	}
 }
