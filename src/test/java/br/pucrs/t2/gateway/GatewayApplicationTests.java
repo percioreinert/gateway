6e899c0dd933c398b298e97f5a8bb09e528d7821
Pércio Reinert <percioreinert@gmail.com>
2021-12-03 17:51:03 -0300

    Initial commit

---
+++ src/test/java/br/pucrs/t2/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.t2.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
